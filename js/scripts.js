4a718e2658392ec3c5f6e483a0e3f7a06b236476
siweik <siweik@amazon.com>
2015-04-30 17:37:28 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// navigation slide-in
+$(window).load(function() {
+  $('.nav_slide_button').click(function() {
+    $('.pull').slideToggle();
+  });
+});
+// fancyBox
+$(document).ready(function() {
+  $(".various").fancybox({
+    maxWidth: 800,
+    maxHeight: 450,
+    fitToView: false,
+    width: '70%',
+    height: '70%',
+    autoSize: false,
+    closeClick: false,
+    openEffect: 'none',
+    closeEffect: 'none'
+  });
+});
